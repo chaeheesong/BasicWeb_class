0d1c47bb3418b5c11103ba61362142ecc67e926b
chaeheesong <129745341+chaeheesong@users.noreply.github.com>
2025-11-02 14:31:40 +0900

    중간 저장

--- brower-extension/dist/main.js
+++ brower-extension/dist/main.js
@@ -1,5 +1,17 @@
 document.querySelector(".form-data"),document.querySelector(".region-name"),document.querySelector(".api-key"),document.querySelector(".errors"),document.querySelector(".loading"),document.querySelector(".result-container"),document.querySelector(".carbon-usage"),document.querySelector(".fossil-fuel"),document.querySelector(".my-region"),document.querySelector(".clear-btn");
 
+const form = document.querySelector(".form-data");
+const region = document.querySelector("#region");
+const apiKey = document.querySelector("#api");
+const errors = document.querySelector(".errors");
+const loading = document.querySelector(".loading");
+const results = document.querySelector(".result");
+const carbonUsage = document.querySelector(".carbon-usage");
+const fossilFuel = document.querySelector(".fossil-fuel");
+const myRegion = document.querySelector(".my-region");
+const clearBtn = document.querySelector(".clear-btn");
+
+
 form.addEventListener('submit', (e) => handleSubmit(e));
 clearBtn.addEventListener('click', (e) => reset(e));
 init();
@@ -38,9 +50,23 @@ function handleSubmit(e) {
 function setUpUser(apiKey, regionName) {
     localStorage.setItem('apiKey', apiKey);
     localStorage.setItem('regionName', regionName);
+
+    form.style.display = 'none';       // form 숨기기
+    results.style.display = 'block';   // 결과 영역 보여주기
     loading.style.display = 'block';
     errors.textContent = '';
     clearBtn.style.display = 'block';
 
     displayCarbonUsage(apiKey, regionName); //다음시간에
+}
+
+// clear 버튼
+function reset(e) {
+    e.preventDefault();
+    localStorage.removeItem('apiKey');
+    localStorage.removeItem('regionName');
+    form.style.display = 'block';
+    results.style.display = 'none';
+    loading.style.display = 'none';
+    errors.textContent = '';
 }
